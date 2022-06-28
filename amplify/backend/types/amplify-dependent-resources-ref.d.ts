d56a8cefbeff81dc3546cfe9fa046281d53b8319
Tom Stamm <stammt@gmail.com>
2022-06-28 15:07:13 -0600

    images

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3photos": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
